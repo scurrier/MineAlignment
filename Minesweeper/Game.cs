e7c24b0c8d759edc014fdd32b3ff10e477e6768a
Shane Currier <shane@enlearn.org>
2016-12-02 10:00:30 -0800

    Win a game, but we don't output it.

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -2,12 +2,27 @@
 {
     public class Game
     {
+        internal Ui Ui { set; private get; }
+
         public static Game Create(int i, int i1)
         {
             return new Game();
         }
 
         public void Play()
+        {
+            Ui.Win();
+        }
+
+        internal Game()
+        {
+            Ui = new Ui();
+        }
+    }
+
+    internal class Ui
+    {
+        public virtual void Win()
         {
         }
     }
--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -2,16 +2,42 @@
 using NUnit.Framework;
 
 namespace Tests
-
 {
     [TestFixture]
     public class GameTests
     {
-        [Test]
-        public void GameCanBePlayed()
+        private class FakeUi : Ui
         {
-            var game = Game.Create(0, 0);
-            Assert.DoesNotThrow(() => game.Play());
+            public bool HasEnded { get; private set; }
+
+            public override void Win()
+            {
+                HasEnded = true;
+            }
+        }
+
+        private Game _testObj;
+        private FakeUi _testObjUi;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testObj = Game.Create(0, 0);
+            _testObjUi = new FakeUi();
+            _testObj.Ui = _testObjUi;
+        }
+
+        [Test]
+        public void RealGameCanBePlayed()
+        {
+            Assert.DoesNotThrow(() => Game.Create(0, 0).Play());
+        }
+
+        [Test]
+        public void GameEnds()
+        {
+            _testObj.Play();
+            Assert.True(_testObjUi.HasEnded);
         }
     }
 }
