4ccfc225749fe0edcb32ee656de5b7327d221081
Shane Currier <shane@enlearn.org>
2016-12-02 09:32:16 -0800

    Game can be played

---
+++ Tests/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using Minesweeper;
+using NUnit.Framework;
+
+namespace Tests
+
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void GameCanBePlayed()
+        {
+            var game = Game.Create(0, 0);
+            Assert.DoesNotThrow(() => game.Play());
+        }
+    }
+}
