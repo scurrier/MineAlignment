83a606d0a5a2191dc93b50ec22fafd6b6e4fbb02
Shane Currier <shane@enlearn.org>
2016-12-15 10:57:15 -0800

    console ui draws board

--- Minesweeper/Board.cs
+++ Minesweeper/Board.cs
@@ -4,19 +4,19 @@ namespace Minesweeper
 {
     public class Board
     {
-        private readonly int _rows;
-        private readonly int _columns;
+        public readonly int Rows;
+        public readonly int Columns;
 
         public Board(int rows, int columns)
         {
-            _rows = rows;
-            _columns = columns;
+            Rows = rows;
+            Columns = columns;
         }
 
         public void CollectCells(Action<Cell> collector)
         {
-            for(var row = 0; row < _rows; row++)
-            for(var col = 0; col < _columns; col++)
+            for(var row = 0; row < Rows; row++)
+            for(var col = 0; col < Columns; col++)
                     collector(new Cell(row, col));
         }
     }
--- Minesweeper/ConsoleUi.cs
+++ Minesweeper/ConsoleUi.cs
@@ -6,11 +6,14 @@ namespace Minesweeper
     {
         public override void EndGame()
         {
+            Console.SetCursorPosition(0, Board.Rows);
             Console.WriteLine("Game over.");
         }
         
         internal override void UpdateCell(Cell cell)
         {
+            Console.SetCursorPosition(cell.Column, cell.Row);
+            Console.Write(".");
         }
     }
 }
